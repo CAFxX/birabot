91faf5c273041fe2168a0cd1749e7a66cdd6f7eb
Carlo Alberto Ferraris <cafxx@strayorange.com>
2013-04-28 14:34:27 +0200

    We'll be using a thermocouple attached directly to the analog input, so let's use the 1.1V internal reference.

--- pins.ino
+++ pins.ino
@@ -1,12 +1,17 @@
 #include "pins.h"
 
 void setup_pins() {
+  // set the ADC reference to 1.1V
+  analogReference(INTERNAL);
+  // set the mode for all pins
+  // LCD pins
   pinMode(PIN_LCD0, OUTPUT);
   pinMode(PIN_LCD1, OUTPUT);
   pinMode(PIN_LCD2, OUTPUT);
   pinMode(PIN_LCD3, OUTPUT);
   pinMode(PIN_LCD4, OUTPUT);
   pinMode(PIN_LCD5, OUTPUT);
+  // keypad pins
   pinMode(PIN_KEYPAD_COL0, INPUT);
   pinMode(PIN_KEYPAD_COL1, INPUT);
   pinMode(PIN_KEYPAD_COL2, INPUT);
@@ -15,10 +20,15 @@ void setup_pins() {
   pinMode(PIN_KEYPAD_ROW1, INPUT);
   pinMode(PIN_KEYPAD_ROW2, INPUT);
   pinMode(PIN_KEYPAD_ROW3, INPUT);
+  // temperature sensor pin (digital)
   pinMode(PIN_TEMP_SENS, INPUT);
+  // flame sensor (thermocouple) pin (analog)
   pinMode(PIN_FLAME_SENS, INPUT);
+  // ignition control pin
   pinMode(PIN_IGNITION, OUTPUT);
+  // gas valve control pin
   pinMode(PIN_GASVALVE, OUTPUT);
+  // unconnected pin, used as entropy source (analog)
   pinMode(PIN_UNCONNECTED, INPUT);
 }
 
